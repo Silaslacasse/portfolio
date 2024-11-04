8457ff7674514b607b04ad44b230294050b6619d
Joss <jocelynduperret@gmail.com>
2024-11-04 11:23:53 +0100

    initialize vite and vue

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+    <header>
+        bonjour
+    </header>
+</template>
+
+<style scoped>
+header {
+    background-color: #27272A;
+}
+
+</style>
